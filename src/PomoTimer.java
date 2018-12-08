639680e679fa673e52f55f0c39bf858f72e15909
Elvis Lam <elvislam502@gmail.com>
2018-12-08 00:47:33 -0800

    add sound then finished

--- src/PomoTimer.java
+++ src/PomoTimer.java
@@ -3,16 +3,15 @@ import java.awt.*;
 import javax.swing.*; //For GUI
 import javax.swing.border.TitledBorder;
 
-import Content.ButtonListener;
 class Content extends JPanel{
-	private JPanel panel1, panel2, panel3, panel4, panel5, panel6, panel7, panel8;
-	private JLabel screen1, screen2, screen3, screen4, screen5, screen6, screen7, screen8;
+	private JPanel panel1, panel2, panel3;
+	private JLabel screen1, screen2;
 	private JButton start, stop, reset;
 	private Timer timer;
-	private int minutesTracker1, secondsCounter1, minutesCounter1, minutes,  minutesTracker2, secondsCounter2, minutesCounter2, minutesTracker3, secondsCounter3, minutesCounter3, minutesTracker4, secondsCounter4, minutesCounter4, minutesTracker5, secondsCounter5, minutesCounter5, minutesTracker6, secondsCounter6, minutesCounter6, minutesTracker7, secondsCounter7, minutesCounter7, minutesTracker8, secondsCounter8, minutesCounter8; 
-	private final int workSecondsDesired = 60;
+	private int minutesTracker1, secondsCounter1, minutesCounter1, minutes,  minutesTracker2, secondsCounter2, minutesCounter2, loopCounter, minutes2; 
+	private final int workSecondsDesired = 1500;
 	private final int workMinutesDesired = workSecondsDesired/60;
-	private final int breakSecondsDesired = 60;
+	private int breakSecondsDesired = 300;
 	private final int breakMinutesDesired = breakSecondsDesired/60;
 
 	
@@ -22,71 +21,34 @@ class Content extends JPanel{
 		
 		panel1 = new JPanel();
 		TitledBorder titled1 = new TitledBorder("Work");
+		titled1.setTitleFont(new Font("Arial", Font.BOLD, 50));
+
 		panel1.setBorder(titled1);
 		screen1 = new JLabel("25m 00s");
+		screen1.setFont(new Font("Arial", Font.PLAIN, 40));
 		panel1.add(screen1);
 		add(panel1);
 		
 		panel2 = new JPanel();
 		TitledBorder titled2 = new TitledBorder("Break");
+		titled2.setTitleFont(new Font("Arial", Font.BOLD, 50));
 		panel2.setBorder(titled2);
 		screen2 = new JLabel("5m 00s");
+		screen2.setFont(new Font("Arial", Font.PLAIN, 40));
 		panel2.add(screen2);
 		add(panel2);
 		
 		panel3 = new JPanel();
-		TitledBorder titled3 = new TitledBorder("Work");
-		panel3.setBorder(titled3);
-		screen3 = new JLabel("25m 00s");
-		panel3.add(screen3);
-		add(panel3);
-		
-		panel4 = new JPanel();
-		TitledBorder titled4 = new TitledBorder("Break");
-		panel4.setBorder(titled4);
-		screen4 = new JLabel("5m 00s");
-		panel4.add(screen4);
-		add(panel4);
-		
-		panel5 = new JPanel();
-		TitledBorder titled5 = new TitledBorder("Work");
-		panel5.setBorder(titled5);
-		screen5 = new JLabel("25m 00s");
-		panel5.add(screen5);
-		add(panel5);
-		
-		panel6 = new JPanel();
-		TitledBorder titled6 = new TitledBorder("Break");
-		panel6.setBorder(titled6);
-		screen6 = new JLabel("5m 00s");
-		panel6.add(screen6);
-		add(panel6);
-		
-		panel7 = new JPanel();
-		TitledBorder titled7 = new TitledBorder("Work");
-		panel7.setBorder(titled7);
-		screen7 = new JLabel("25m 00s");
-		panel7.add(screen7);
-		add(panel7);
-		
-		
-		
-		panel8 = new JPanel();
-		TitledBorder titled8 = new TitledBorder("Break");
-		panel8.setBorder(titled8);
-		screen8 = new JLabel("5m 00s");
-		panel8.add(screen8);
-		add(panel8);
-		
 		start = new JButton("Start");
 		start.addActionListener(new ButtonListener());
-		add(start);
-		stop = new JButton("Stop");
+		panel3.add(start);
+		stop = new JButton("Pause");
 		stop.addActionListener(new ButtonListener());
-		add(stop);
+		panel3.add(stop);
 		reset = new JButton("Reset");
 		reset.addActionListener(new ButtonListener());
-		add(reset);
+		panel3.add(reset);
+		add(panel3);
 		
 	}
 	private class ButtonListener implements ActionListener{
@@ -95,12 +57,21 @@ class Content extends JPanel{
 				timer.stop();
 			} else if (event.getSource() == reset){
 				timer.stop();
+				panel1.setBackground(Color.white);
+				panel2.setBackground(Color.white);
 				minutesTracker1 = 0;
 				secondsCounter1 = 0;
-				screen1.setText("0m 00s");
+				minutesTracker2 = 0;
+				secondsCounter2 = 0;
+				minutesTracker1 = 0;
+				minutesCounter1 = 0;
+				minutesTracker2 = 0;
+				minutesCounter2 = 0;
+				screen1.setText("25m 00s");
+				screen2.setText("5m 00s");
 			} else { //can't be else if start
 				timer.start();
-				if(secondsCounter1 != 60){
+				if(secondsCounter1 != 60 || minutes <0){
 					panel1.setBackground(Color.green);
 					minutesTracker1++;
 					minutes = (workSecondsDesired-minutesTracker1)/60;
@@ -112,102 +83,35 @@ class Content extends JPanel{
 				}
 				if(minutesCounter1 == breakMinutesDesired){
 					panel1.setBackground(Color.white);
-					screen1.setText("Finished");
-					if(secondsCounter2 != 60){
+					screen1.setText("25m 00s");
+					if(secondsCounter2 != 60 || minutes2 <0){
 						panel2.setBackground(Color.red);
 						minutesTracker2++;
-						minutes = (breakSecondsDesired-minutesTracker2)/60;
-						screen2.setText(String.valueOf(minutes + "m " + (60-secondsCounter2) +"s"));
+						minutes2 = (breakSecondsDesired-minutesTracker2)/60;
+						screen2.setText(String.valueOf(minutes2 + "m " + (60-secondsCounter2) +"s"));
 						secondsCounter2++;
 					} else {
+						minutesTracker1 = 0;
+						secondsCounter1 = 0;
+						minutesTracker2 = 0;
 						secondsCounter2 = 0;
-						minutesCounter2++;
-					}
-					if(minutesCounter2 == workMinutesDesired){
+						minutesTracker1 = 0;
+						minutesCounter1 = 0;
+						minutesTracker2 = 0;
+						minutesCounter2 = 0;
 						panel2.setBackground(Color.white);
-						screen2.setText("Finished");
-						if(secondsCounter3 != 60){
-							panel3.setBackground(Color.green);
-							minutesTracker3++;
-							minutes = (workSecondsDesired-minutesTracker3)/60;
-							screen3.setText(String.valueOf(minutes + "m " + (60-secondsCounter3) +"s"));
-							secondsCounter3++;
-						} else {
-							secondsCounter3 = 0;
-							minutesCounter3++;
-						}
-						if(minutesCounter3 == breakMinutesDesired){
-							panel3.setBackground(Color.white);
-							screen3.setText("Finished");
-							if(secondsCounter4 != 60){
-								panel4.setBackground(Color.red);
-								minutesTracker4++;
-								minutes = (breakSecondsDesired-minutesTracker4)/60;
-								screen2.setText(String.valueOf(minutes + "m " + (60-secondsCounter4) +"s"));
-								secondsCounter4++;
-							} else {
-								secondsCounter4 = 0;
-								minutesCounter4++;
-							}
-							if(minutesCounter4 == workMinutesDesired){
-								panel4.setBackground(Color.white);
-								screen4.setText("Finished");
-								if(secondsCounter5 != 60){
-									panel5.setBackground(Color.green);
-									minutesTracker5++;
-									minutes = (workSecondsDesired-minutesTracker5)/60;
-									screen5.setText(String.valueOf(minutes + "m " + (60-secondsCounter5) +"s"));
-									secondsCounter5++;
-								} else {
-									secondsCounter5 = 0;
-									minutesCounter5++;
-								}
-								if(minutesCounter5 == breakMinutesDesired){
-									panel5.setBackground(Color.white);
-									screen5.setText("Finished");
-									if(secondsCounter6 != 60){
-										panel6.setBackground(Color.red);
-										minutesTracker6++;
-										minutes = (breakSecondsDesired-minutesTracker6)/60;
-										screen6.setText(String.valueOf(minutes + "m " + (60-secondsCounter6) +"s"));
-										secondsCounter6++;
-									} else {
-										secondsCounter6 = 0;
-										minutesCounter6++;
-									}
-									if(minutesCounter6 == workMinutesDesired){
-										panel5.setBackground(Color.white);
-										screen5.setText("Finished");
-										if(secondsCounter7 != 60){
-											panel7.setBackground(Color.green);
-											minutesTracker7++;
-											minutes = (workSecondsDesired-minutesTracker7)/60;
-											screen5.setText(String.valueOf(minutes + "m " + (60-secondsCounter7) +"s"));
-											secondsCounter7++;
-										} else {
-											secondsCounter7 = 0;
-											minutesCounter7++;
-										}
-										if(minutesCounter7 == breakMinutesDesired){
-											panel7.setBackground(Color.white);
-											screen7.setText("Finished");
-											if(secondsCounter8 != 60){
-												panel8.setBackground(Color.red);
-												minutesTracker8++;
-												minutes = (breakSecondsDesired-minutesTracker8)/60;
-												screen8.setText(String.valueOf(minutes + "m " + (60-secondsCounter8) +"s"));
-												secondsCounter8++;
-											} else {
-												secondsCounter8 = 0;
-												minutesCounter8++;
-											}
-										}
-									}
-								}
-							}
-						}
+						screen2.setText("5m 00s");
+						loopCounter++;
+						if(loopCounter ==3){
+							screen2.setText("15m 00s");
+							breakSecondsDesired = 900;
+						} 
 					}
-				}	
+				}
+				if (loopCounter == 4){
+					loopCounter = 0;
+					breakSecondsDesired = 60;
+				}
 			}
 		}
 	}	
@@ -215,7 +119,7 @@ class Content extends JPanel{
 public class PomoTimer {
 	public static void main(String[] args) {
 		//Sets up the window for the GUI
-		JFrame frame = new JFrame("Pomodoro Timer by Elam"); //Title of window
+		JFrame frame = new JFrame("Elam's P.T"); //Title of window
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//Adds content to GUI
 		Content panel = new Content();
@@ -223,7 +127,7 @@ public class PomoTimer {
 		//Allows it to open when executed
 		frame.pack();
 		frame.setVisible(true);
-		//frame.setResizable(false); //Disables window resize
+		frame.setResizable(false); //Disables window resize
 
 	}
 
